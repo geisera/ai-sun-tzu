39cc0f3ea20a71f9761ba333ac0ae82a2f4e0145
Tony <geisera@gmail.com>
2023-09-19 11:15:31 -0500

    UI change

--- app/page.tsx
+++ app/page.tsx
@@ -41,9 +41,9 @@ export default function Home() {
 
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h3 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             What do you want to know about M&S?
-          </h2>
+          </h3>
 
             <div>
               <input className="block w-full rounded-md border-0 my-5 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
