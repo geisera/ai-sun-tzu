8c9f978b3cdb0996bbaa85a667c3db2a7acea3a8
Tony <geisera@gmail.com>
2023-09-19 10:59:26 -0500

    updated ui

--- app/page.tsx
+++ app/page.tsx
@@ -37,16 +37,37 @@ export default function Home() {
   }
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <input className='text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <button className="px-7 py-1 rounded-2xl bg-white text-black mt-2 mb-2" onClick={sendQuery}>Ask AI</button>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label className="block text-md font-medium leading-6 text-gray-300">
+                What do you want to know?
+              </label>
+            </div>
+
+            <div>
+              <input className="block rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={e => setQuery(e.target.value)} />
+            </div>
+            <div>
+                <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={sendQuery}>Ask M&S</button>
+            </div>
+            
+          </form>
+        </div>
+      </div>
+
       {
-        loading && <p>Asking AI ...</p>
+        loading && <p>Asking M&S handbook ...</p>
       }
       {
         result && <p>{result}</p>
       }
-      { /* consider removing this button from the UI once the embeddings are created ... */}
-      <button onClick={createIndexAndEmbeddings}>Create index and embeddings</button>
+      {/* consider removing this button from the UI once the embeddings are created ... */}
+      {/* <button onClick={createIndexAndEmbeddings}>Create index and embeddings</button> */}
     </main>
   )
 }
