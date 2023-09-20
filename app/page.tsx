8a7beb6298d1abd22f2df1159a13dfe17dc07ccd
Tony <geisera@gmail.com>
2023-09-20 10:54:57 -0500

    changed button text

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,8 @@ export default function Home() {
                   onChange={e => setQuery(e.target.value)} />
             </div>
             <div>
-                <button className="flex w-full justify-center rounded-md bg-blue-900 mb-5 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={sendQuery}>What do we want to know?</button>
+                <button className="flex w-full justify-center rounded-md bg-blue-900 mb-5 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={sendQuery}>
+                  What do you want to know?</button>
             </div>
             
         </div>
